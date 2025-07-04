5f2a31950da5760efe7c7a38c2e8c87e51af772b
RonaldAriasDev <r44ms4@gmail.com>
2025-07-03 21:57:27 -0400

    Agregar script calculadora.py
    Calculadora básica con soporte para suma, resta, multiplicación y división, incluyendo validación de entradas y manejo de errores.

---
+++ Calculadora.py
@@ -0,0 +1,27 @@
+# Calculadora Básica con operadores +, -, *, /
+
+print("CALCULADORA BÁSICA. Use operadores +, -, *, /")
+numero = input("ingrese un número: ")
+operador = input("ingrese un operador: ")
+numero1 = input("ingrese un número: ")
+
+try:
+  numero_int = int(numero)
+  numero1_int = int(numero1)
+  if operador == "+":
+    print(numero_int + numero1_int)
+  elif operador == "-":
+    print(numero_int - numero1_int)
+  elif operador == "*":
+    print(numero_int * numero1_int)
+  elif operador == "/":
+    try:
+      print(numero_int / numero1_int)
+    except ZeroDivisionError:
+      print("No es posible dividir entre 0")
+  else:
+    print("operador invalido")
+except ValueError:
+  print("Los valores ingresados deben ser números enteros")
+
+
