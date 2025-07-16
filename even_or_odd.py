244426d0f6dbde0c502e5cef0de99b7def8a3c86
RonaldAriasDev <r44ms4@gmail.com>
2025-07-16 00:32:43 -0400

    Refactored variable names to English for better readability

--- even_or_odd.py
+++ even_or_odd.py
@@ -1,15 +1,13 @@
 # Crea un programa que pida un número al usuario y devuelva si es par o impar
 
 print("PAR O IMPAR")
-
-numero = input("Ingrese un número: ").strip()
+number = input("Ingrese un número: ").strip()
 
 try:
-  numero_int = int(numero)
-  if (numero_int % 2) == 0:
-    print(f"El número {numero_int} es par")
+  number_int = int(number)
+  if (number_int % 2) == 0:
+    print(f"El número {number_int} es par")
   else:
-    print(f"El número {numero_int} es impar")
+    print(f"El número {number_int} es impar")
 except ValueError:
   print("por favor ingrese un número entero, no decimal, ni en texto")
-
