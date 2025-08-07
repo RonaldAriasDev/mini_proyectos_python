a8430047fe8a37cf39feff941e9e706964b2fd3a
RonaldAriasDev <r44ms4@gmail.com>
2025-08-07 00:52:51 -0400

    Fix long lines in exercise description in classes.py

--- classes.py
+++ classes.py
@@ -1,6 +1,8 @@
 '''
-Implementa dos clases que representen las estructuras de Pila y Cola (estudiadas en el ejercicio número 7 de la ruta de estudio)
-Deben poder inicializarse y disponer de operaciones para añadir, eliminar, retornar el número de elementos e imprimir todo su contenido.
+Implementa dos clases que representen las estructuras de Pila y Cola (estudiadas
+en el ejercicio número 7 de la ruta de estudio)
+Deben poder inicializarse y disponer de operaciones para añadir, eliminar,
+retornar el número de elementos e imprimir todo su contenido.
  '''
  
  
