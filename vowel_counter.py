6ebe8289271c406fdfebe660b2746e92b75796ee
RonaldAriasDev <r44ms4@gmail.com>
2025-08-17 13:09:52 -0400

    Update vowel counter description

--- vowel_counter.py
+++ vowel_counter.py
@@ -1,3 +1,9 @@
+# CONTADOR DE VOCALES
+# El programa solicita al usuario ingresar una palabra.
+# Convierte la palabra a minúsculas y reemplaza las vocales acentuadas.
+# Recorre cada letra e indica si es vocal o consonante.
+# Al final, muestra la cantidad total de vocales en la palabra ingresada.
+
 print("CONTADOR DE VOCALES")
 word = input("Ingrese una palabra: ")
 format_word = word.lower().strip().replace("á","a").replace("é", "e").replace("í","i").replace("ó", "o").replace("ú", "u")
@@ -9,6 +15,6 @@ for letter in format_word:
     print(f"{letter}: es una vocal")
     vowel_count += 1
   else:
-    print(letter)
+    print(f"{letter} es una consonante")
 
-print(f"Cantidad de vocales en {word}: {vowel_count}")
+print(f" \nCantidad de vocales en {word}: {vowel_count}")
