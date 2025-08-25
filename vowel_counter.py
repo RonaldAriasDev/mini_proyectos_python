f953438227d3539df06d7cb64f697785788ea46a
RonaldAriasDev <r44ms4@gmail.com>
2025-08-25 03:16:23 -0400

    Refactor program logic and add input validation

--- vowel_counter.py
+++ vowel_counter.py
@@ -1,20 +1,39 @@
 # CONTADOR DE VOCALES
 # El programa solicita al usuario ingresar una palabra.
-# Convierte la palabra a minúsculas y reemplaza las vocales acentuadas.
+# Reemplaza las vocales acentuadas y convierte la palabra a minúsculas.
 # Recorre cada letra e indica si es vocal o consonante.
 # Al final, muestra la cantidad total de vocales en la palabra ingresada.
 
+
+def validate_prompt(prompt: str):
+  while True:
+    text = input(prompt).strip()
+    if text.isalpha():
+      return (
+        text.replace('á','a')
+            .replace('é', 'e')
+            .replace('í','i')
+            .replace('ó', 'o')
+            .replace('ú', 'u')
+            .lower()
+            )
+    else:
+      print('Ingrese solo letras')
+
+
 print("CONTADOR DE VOCALES")
-word = input("Ingrese una palabra: ")
-format_word = word.lower().strip().replace("á","a").replace("é", "e").replace("í","i").replace("ó", "o").replace("ú", "u")
-vowels = ["a", "e", "i", "o", "u"]
-vowel_count = 0
+vowels = ['a', 'e', 'i', 'o', 'u']
 
-for letter in format_word:
-  if letter in vowels:
-    print(f"{letter}: es una vocal")
-    vowel_count += 1
-  else:
-    print(f"{letter} es una consonante")
+while True:
+  validated_prompt = validate_prompt('' \nIngrese una palabra: ')
+  if validated_prompt == ''
+  vowels_count = 0
+  
+  for letter in validated_prompt:
+    if letter in vowels:
+      print(f'{letter}: es una vocal')
+      vowels_count += 1
+    else:
+      print(f'{letter} es una consonante')
 
-print(f" \nCantidad de vocales en {word}: {vowel_count}")
+  print(f'Cantidad de vocales en {validated_prompt}: {vowels_count}')
