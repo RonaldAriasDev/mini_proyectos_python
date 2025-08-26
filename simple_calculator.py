1a2f8a9d78222c38219979f77253d711b935af5f
RonaldAriasDev <r44ms4@gmail.com>
2025-08-26 01:27:18 -0400

    Modularize and improve program flow, and add new input validations

--- simple_calculator.py
+++ simple_calculator.py
@@ -2,14 +2,33 @@
 # El programa solicita al usuario los dos números y el operador a utilizar, incluye validación de entradas y muestra el resultado de la operación.
 
 
-print("CALCULADORA BÁSICA")
-first_input = input("ingrese un número: ")
-operator = input("ingrese un operador (+, -, *, /): ")
-second_input = input("ingrese un número: ")
+def validate_number(prompt: str):
+  while True:
+    try:
+      validated_number = int(input(prompt).strip())
+      return validated_number
+    except ValueError:
+      print('Ingrese un número entero')
 
-try:
-  first_number = int(first_input)
-  second_number = int(second_input)
+
+def validate_operator(prompt: str):
+  while True:
+    operator = input(prompt).strip()
+    operators = ['+', '-', '*', '/']
+    if operator in operators:
+      return operator
+    else:
+      print('Ingrese un operador válido')
+
+
+print("CALCULADORA BÁSICA")
+
+while True:
+  print(' ')
+  first_number = validate_number("ingrese un número: ")
+  operator = validate_operator("ingrese un operador (+, -, *, /): ")
+  second_number = validate_number("ingrese un número: ")
+  
   if operator == "+":
     print(first_number + second_number)
   elif operator == "-":
@@ -21,8 +40,3 @@ try:
       print(first_number / second_number)
     except ZeroDivisionError:
       print("No es posible dividir entre 0")
-  else:
-    print("operador invalido")
-except ValueError:
-  print("Los valores ingresados deben ser números entero
-  s")
