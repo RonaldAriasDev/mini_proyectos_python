acf718312b6f0895e1cf5d8d20091753f8dfaa3d
RonaldAriasDev <r44ms4@gmail.com>
2025-08-28 01:50:27 -0400

    Modularize and improve program flow

--- browser_navigation_simulation.py
+++ browser_navigation_simulation.py
@@ -1,39 +1,57 @@
-# 📌 Simulación del mecanismo adelante/atrás de un navegador web
-# Utilizando pila y cadenas de texto:
-# - Las palabras "adelante" y "atrás" desplazan en la historia de navegación.
-# - Cualquier otro texto se interpreta como el nombre de una nueva web.
-# - El programa muestra el nombre de la página actual en cada acción.
+# 📌 Simulación del mecanismo de navegación (atrás/adelante) de un navegador web
+# Valida que la entrada del usuario sea un número entero y esté dentro del rango de las opciones disponibles.
+# Utiliza pilas para implementar las siguientes acciones:
+# Ingresar a una página.
+# Ir atrás.
+# Ir adelante. 
 
 
-def interface():
-  opcion = input(" \nBrowser\nPara ingresar una nueva dirección digite 1\nPara"
-  "regresar ingrese 2\nPara ir hacia adelante ingrese 3: \n ")
-  return opcion
+def validate_prompt():
+  options = [
+    '1. Buscar', 
+    '2. Atrás', 
+    '3. Adelante: '
+    ]
+  while True:
+    try:
+      option = int(input(' \n' 'Ingrese una opción\n' + '\n'.join(options)).strip())
+      if 1 <= option <= 3:
+        return option
+      else:
+        print('Opción no válida')
+    except ValueError:
+      print('Ingrese un número entero')
+
+
+def enter_url():
+  web_page = input('Ingresar dirección web: ').strip()
+  return web_page
  
-
-history1 = ['inicio']  
-history2 = []
+ 
+print('Firefox')
+my_list1 = []  
+my_list2 = []
 
 while True:
-  opcion = interface()
+  option = validate_prompt()
   
-  match opcion:
-    case '1':
-      history1.append(input('Ingresar dirección: '))
-      history2.clear()
-      print(f'Bienvenido a {history1[-1]}')
-    case '2':
-      if len(history1) >= 2:
-        history2.append(history1.pop())
-        print(f'Bienvenido a {history1[-1]}')
+  match option:
+    case 1:
+      web_page = enter_url()
+      my_list1.append(web_page)
+      my_list2.clear()
+      print(f'Estás en la página de {my_list1[-1]}')
+    case 2:
+      if len(my_list1) >= 2:
+        my_list2.append(my_list1.pop())
+        print(f'Estás en la página de  {my_list1[-1]}')
       else:
         print('No es posible ir atrás')
-    case '3':
-      if len(history2) >= 1:
-        history1.append(history2.pop())
-        print(f'Bienvenido a {history1[-1]}')
+    case 3:
+      if len(my_list2) >= 1:
+        my_list1.append(my_list2.pop())
+        print(f'Estás en la página de {my_list1[-1]}')
       else:
         print('No es posible ir adelante')
     case _:
-      print('Opción no valid
-      a')
+      pass
