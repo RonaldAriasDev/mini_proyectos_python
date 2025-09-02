fe06ffa5d7644b314e57e8009e585977863f61e5
RonaldAriasDev <r44ms4@gmail.com>
2025-09-02 18:55:46 -0400

    Clean and improve program flow and update docstrings for consistency and best practices

--- phone_book.py
+++ phone_book.py
@@ -1,116 +1,119 @@
-# 📌 Agenda de contactos por terminal
-# El programa permite gestionar una lista de contactos.
-# El programa solicita al usuario qué operación desea realizar y verifica que la opción seleccionada sea sea válida
-# Funcionalidades:
-# Mostrar todos los contactos.
-# Buscar un contacto por nombre.
-# Añadir un nuevo contacto con nombre y número de teléfono. Los números no deben superar los 9 dígitos.
-# Actualizar el número de un contacto existente.
-# Eliminar un contacto.
-# Finalizar el programa.
+"""
+Gestiona una agenda de contactos por terminal.
+
+Permite realizar distintas operaciones sobre la lista de contactos:
+- Mostrar todos los contactos.
+- Buscar un contacto por nombre.
+- Añadir un nuevo contacto (número máximo: 9 dígitos).
+- Actualizar el número de un contacto existente.
+- Eliminar un contacto.
+- Finalizar el programa.
+"""
 
 
 def validate_menu_option():
-  my_list = [
-    'Seleccione una opción:', 
-    '1. Mostrar contactos', 
-    '2. Buscar por nombre', 
-    '3. Agregar', 
-    '4. Actualizar', 
-    '5. Eliminar', 
-    '6. Salir: '
+    my_list = [
+        'Seleccione una opción:', 
+        '1. Mostrar contactos', 
+        '2. Buscar por nombre', 
+        '3. Agregar', 
+        '4. Actualizar', 
+        '5. Eliminar', 
+        '6. Salir: '
     ]
-  
-  while True:
-    try:
-      menu_option = int(input(' \n' + '\n'.join(my_list)).strip())
-      if 1 <= menu_option <= 6:
-        return menu_option
-      else:
-        print('Ingrese una opción válida')
-    except ValueError:
-      print('Ingrese un número entero')
+    
+    while True:
+        try:
+            menu_option = int(input('\n' + '\n'.join(my_list)).strip())
+            if 1 <= menu_option <= 6:
+                return menu_option
+            else:
+                print('Ingrese una opción válida')
+        except ValueError:
+            print('Ingrese un número entero')
   
 
 def show_contacts():
-  if len(contacts) >= 1:
-    print(contacts)
-  else:
-    print('No hay contactos registrados')
+    if len(contacts) >= 1:
+        print(contacts)
+    else:
+        print('No hay contactos registrados')
   
 
 def validate_name():
-  while True:
-    validated_name = input('Ingrese un nombre: ').strip()
-    if all(word.isalpha() for word in validated_name.split()):
-      return validated_name.title()
-    else:
-      print('Ingrese solo letras')
+    while True:
+        validated_name = input('Ingrese un nombre: ').strip()
+        if all(word.isalpha() for word in validated_name.split()):
+            return validated_name.title()
+        else:
+            print('Ingrese solo letras')
 
   
 def search_contact(name):
-  if name in contacts:
-    print(f'El número de {name} es {contacts[name]}')
-  else:
-    print('Contacto no encontrado')
- 
- 
+    if name in contacts:
+        print(f'El número de {name} es {contacts[name]}')
+    else:
+        print('Contacto no encontrado')
+  
+
 def validate_number():
-  while True:
-    try:
-      validated_number = int(input('Ingrese un número de nueve digitos como máximo: ').strip())
-      if 1 <= len(str(validated_number)) <= 9:
-        return validated_number
-      else:
-        print('Ingrese entre 1 y 9 dígitos')
-    except ValueError:
-      print('Ingrese un número entero')
+    while True:
+        try:
+            validated_number = int(
+                input('Ingrese un número de nueve digitos como máximo: ').strip()
+            )
+            if 1 <= len(str(validated_number)) <= 9:
+                return validated_number
+            else:
+                print('Ingrese entre 1 y 9 dígitos')
+        except ValueError:
+            print('Ingrese un número entero')
  
  
 def add_contact(name, number):
-  contacts[name] = number
-  print('Contacto guardado correctamente')
+    contacts[name] = number
+    print('Contacto guardado correctamente')
 
 
 def update_contact(name, number):
-  contacts[name] = number
-  print('Contacto actualizado correctamente')
+    contacts[name] = number
+    print('Contacto actualizado correctamente')
   
 
 def delete_contact(name):
-  if name in contacts:
-    del contacts[name]
-    print('Contacto eliminado correctamente')
-  else:
-    print('Contacto no encontrado')
+    if name in contacts:
+        del contacts[name]
+        print('Contacto eliminado correctamente')
+    else:
+        print('Contacto no encontrado')
 
 
 def exit():
-  print('Hasta pronto')
+    print('Hasta pronto')
 
 
 print("AGENDA TELEFÓNICA")
 contacts = {}
 
 while True:
-  menu_option = validate_menu_option()
-  
-  if menu_option == 1:
-    show_contacts()
-  elif menu_option == 2:
-    name = validate_name()
-    search_contact(name)
-  elif menu_option == 3:
-    name = validate_name()
-    number = validate_number()
-    add_contact(name, number)
-  elif menu_option == 4:
-    name = validate_name()
-    number = validate_number()
-    update_contact(name, number)
-  elif menu_option == 5:
-    name = validate_name()
-    delete_contact(name)
-  elif menu_option == 6:
-    exit()
-    break
+    menu_option = validate_menu_option()
+    
+    if menu_option == 1:
+        show_contacts()
+    elif menu_option == 2:
+        name = validate_name()
+        search_contact(name)
+    elif menu_option == 3:
+        name = validate_name()
+        number = validate_number()
+        add_contact(name, number)
+    elif menu_option == 4:
+        name = validate_name()
+        number = validate_number()
+        update_contact(name, number)
+    elif menu_option == 5:
+        name = validate_name()
+        delete_contact(name)
+    elif menu_option == 6:
+        exit()
+        break
